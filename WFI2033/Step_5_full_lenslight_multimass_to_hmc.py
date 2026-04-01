7a9ecbfc836b38da35f911f6fd8e540073286304
Tian Li <tianli@gpu01.pri.sciama.icg.port>
2026-04-01 11:37:14 +0100

    test

--- WFI2033/Step_5_full_lenslight_multimass_to_hmc.py
+++ WFI2033/Step_5_full_lenslight_multimass_to_hmc.py
@@ -390,7 +390,7 @@ def build_model_components_with_fixed_lens_light(
 
 def scale_theta_E_from_g2(theta_E_g2, target_prior):
     return (
-        (theta_E_g2 - SIS_G2_PRIOR["theta_mean"])/SIS_G2_PRIOR["theta_mean"]*target_prior["theta_mean"]
+        (theta_E_g2 - SIS_G2_PRIOR["theta_mean"])/SIS_G2_PRIOR["theta_mean"]*target_prior["theta_mean"]+target_prior["theta_mean"]
     )
 
 
