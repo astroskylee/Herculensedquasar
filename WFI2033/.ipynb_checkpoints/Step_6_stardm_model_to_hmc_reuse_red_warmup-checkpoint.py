305a1517fa75c7ed70ab4596880dee77c590ec1f
Tian Li <tianli@node450.pri.sciama.icg.port>
2026-05-11 12:56:53 +0100

    test

--- WFI2033/.ipynb_checkpoints/Step_6_stardm_model_to_hmc_reuse_red_warmup-checkpoint.py
+++ WFI2033/.ipynb_checkpoints/Step_6_stardm_model_to_hmc_reuse_red_warmup-checkpoint.py
@@ -36,8 +36,8 @@ mass_model_base.STRING_MAPPING['CUSPY_NFW_ELLIPSE_KAPPA'] = CuspyNFWEllipseKappa
 
 suffix = '_ss=2_fullconcen_light_multimass'
 run_tag = '20260427_14'
-STEP3_COSMO_PRIOR = 'PantheonSH0ES'  # or 'DESI_PLANCK'
-RED_WARMUP_SOURCE_TAG = '20260502_06'
+STEP3_COSMO_PRIOR = 'DESI_PLANCK'  # or 'DESI_PLANCK'
+RED_WARMUP_SOURCE_TAG = '20260504_16'# or 0502_06
 RED_WARMUP_CHAIN_INDEX = 3  # matplotlib C3, the red chain
 RESUME_RUN_TAG = None  # e.g. '20260408_15' to append more HMC batches to an existing Step-6 run
 resume_mode = RESUME_RUN_TAG is not None
